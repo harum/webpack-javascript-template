9d4bb5666757b561620cc11eebabea42210fc5c9
Putu Harum Bawa <lightharum.108@gmail.com>
2020-02-23 16:11:43 +0700

    add css loader

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import add from './add';
+import './style.css';
 
 const number = add(4, 5);
 
 console.log('number', number);
-document.body.innerHTML = number;
+document.body.innerHTML = `<div class="container">${number}</div>`;
